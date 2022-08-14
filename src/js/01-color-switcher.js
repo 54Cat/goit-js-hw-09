50baa1ba4d404f5af3c42ef9c1ab9abacf5bfc16
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-08-14 21:26:20 +0300

    bugfix#1
    внесла правки в задачи 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,3 @@
-// Задание 1 - переключатель цветов
-
-// Выполняй это задание в файлах 01-color-switcher.html и 01-color-switcher.js. Посмотри демо видео работы переключателя.
-
-// В HTML есть кнопки «Start» и «Stop».
-
-// <button type="button" data-start>Start</button>
-// <button type="button" data-stop>Stop</button>
-
-// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на случайное значение используя инлайн стиль. При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
-
-// ВНИМАНИЕ
-// Учти, на кнопку «Start» можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled).
-
-// Для генерации случайного цвета используй функцию getRandomHexColor.
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-
-
 const btnStart = document.querySelector("[data-start]");
 const btnStop = document.querySelector("[data-stop]");
 let intervalId = null;
@@ -34,7 +12,7 @@ function onStartSwitchColor() {
     btnStart.disabled = true;
 
     intervalId = setInterval(() => {
-        let color = getRandomHexColor();
+        const color = getRandomHexColor();
         document.body.style.backgroundColor = color;
     }, 1000); 
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,84 +1,29 @@
-// Задание 3 - генератор промисов
-// Выполняй это задание в файлах 03-promises.html и 03-promises.js. Посмотри демо видео работы генератора промисов.
-
-// В HTML есть разметка формы, в поля которой пользователь будет вводить первую задержку в миллисекундах, шаг увеличения задержки для каждого промиса после первого и количество промисов которое необходимо создать.
-
-// <form class="form">
-//   <label>
-//     First delay (ms)
-//     <input type="number" name="delay" required />
-//   </label>
-//   <label>
-//     Delay step (ms)
-//     <input type="number" name="step" required />
-//   </label>
-//   <label>
-//     Amount
-//     <input type="number" name="amount" required />
-//   </label>
-//   <button type="submit">Create promises</button>
-// </form>
-
-// Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, сколько ввели в поле amount. При каждом вызове передай ей номер создаваемого промиса (position) и задержку учитывая введенную пользователем первую задержку (delay) и шаг (step).
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-// Дополни код функции createPromise так, чтобы она возвращала один промис, который выполянется или отклоняется через delay времени. Значением промиса должен быть объект, в котором будут свойства position и delay со значениями одноименных параметров. Используй начальный код функции для выбора того, что нужно сделать с промисом - выполнить или отклонить.
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-// Библиотека уведомлений
-// ВНИМАНИЕ
-// Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
-
-// Для отображения уведомлений пользователю вместо console.log() используй библиотеку notiflix.
-
-
-
 import Notiflix from 'notiflix';
 
 const delayEL = document.querySelector('[name=delay]');
 const stepEl = document.querySelector('[name=step]');
 const amountEl = document.querySelector('[name=amount]');
 const btnSubmit = document.querySelector('button');
-// const positions = [];
 
 btnSubmit.addEventListener('click', onStartCreatePromise);
 
 function onStartCreatePromise(e) {
   e.preventDefault();
 
-  let delay = Number(delayEL.value);
-  const step = Number(stepEl.value);
+  const firstDelay = Number(delayEL.value);
+  const stepDelay = Number(stepEl.value);
   const amount = Number(amountEl.value);
+  let delay = firstDelay;
 
   for (position = 1; position <= amount; position++) {
-    createPromise(position, delay).then(onCreatePromiseSuccess).catch(onCreatePromiseError);
-    delay += step;
-    // positions.push(i);
+    createPromise(position, delay).then(lookResultFulfilled).catch(lookResultRejected);
+    delay += stepDelay;
   }
-
-  // const promises = positions.map((position, delay) => createPromise(position, delay));
-  // Promise.all(promises).then(onCreatePromiseSuccess).catch(onCreatePromiseError);
 };
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    console.log(shouldResolve);
   
     setTimeout(() => {
       if (shouldResolve) {
@@ -89,10 +34,10 @@ function createPromise(position, delay) {
   })
 };
 
-function onCreatePromiseSuccess(result) {
+function lookResultFulfilled(result) {
   Notiflix.Notify.success(result);
 };
 
-function onCreatePromiseError(error) {
+function lookResultRejected(error) {
   Notiflix.Notify.failure(error);
 };
