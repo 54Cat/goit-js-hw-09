d74db73ed714a7ba885e018d1a43ba7bec1fc31c
54Cat <97835613+54Cat@users.noreply.github.com>
2022-08-14 13:12:26 +0000

    Deploying to gh-pages from @ 54Cat/goit-js-hw-09@acd8a4115e417c15dce4e442e1896fdf97435ac7 🚀

---
+++ 01-color-switcher.2c6f69de.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;e.disabled=!0,t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,a=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.2c6f69de.js.map
